8313a69b74bcaa16c1a436787835e5c345a1fe24
Italo Paiva <italo.paiva.b@gmail.com>
2018-04-19 23:13:06 +0000

    Adjustments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :orders
+
+  validates :name, uniqueness: { message: 'Já existe um usuário com esse nome. Por favor, crie com outro nome.' }
 end
